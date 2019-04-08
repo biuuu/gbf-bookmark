6fd645a85fd08e65019db776be0c1a21ea72dc12
umisuna <blhx@danmu9.com>
2019-04-08 12:54:28 +0800

    Updates

--- gbf-bookmark.user.js
+++ gbf-bookmark.user.js
@@ -15,45 +15,45 @@
 
   var data = {
     list: [{
-      url: '#mypage',
-      name: '首页',
-      index: 3,
-      background: '#297fc8'
+      "url": "#mypage",
+      "name": "首页",
+      "index": 3,
+      "background": "#297fc8"
     }, {
-      url: '#quest/assist',
-      name: '副本列表',
-      index: 7,
-      background: '#c96883'
+      "url": "#quest/assist",
+      "name": "副本列表",
+      "index": 7,
+      "background": "#c96883"
     }, {
-      url: '#quest/assist/event',
-      name: '活动副本',
-      index: 8,
-      background: '#8dc3dd'
+      "url": "#quest/assist/event",
+      "name": "活动副本",
+      "index": 8,
+      "background": "#8dc3dd"
     }, {
-      url: '#quest/fate',
-      name: 'Fate',
-      index: 9,
-      background: '#fff'
+      "name": "Fate",
+      "url": "#quest/fate",
+      "background": "#efb983",
+      "index": 9
     }, {
-      url: '#sidestory',
-      name: 'SIDE STORY',
-      index: 10,
-      background: '#eee3c8'
+      "url": "#sidestory",
+      "name": "SIDE STORY",
+      "index": 10,
+      "background": "#eee3c8"
     }, {
-      url: '#arcarum2',
-      name: '塔罗首页',
-      index: 12,
-      background: '#ff972d'
+      "name": "塔罗首页",
+      "url": "#arcarum2",
+      "background": "#5fcd70",
+      "index": 12
     }, {
-      url: 'back',
-      name: '后退',
-      index: 16,
-      background: '#FFEB3B'
+      "url": "back",
+      "name": "后退",
+      "index": 16,
+      "background": "#FFEB3B"
     }, {
-      url: 'reload',
-      name: '刷新',
-      index: 18,
-      background: '#5fc829'
+      "url": "reload",
+      "name": "刷新",
+      "index": 18,
+      "background": "#de3a7c"
     }]
   };
 
